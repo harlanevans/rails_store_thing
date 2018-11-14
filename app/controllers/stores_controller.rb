e6c94937c4046b71867130aaa164cfa1fa9c15ce
Harlan Evans <harlanevans5@gmail.com>
2018-11-14 16:44:24 -0700

    commiting

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,13 +1,55 @@
 class StoresController < ApplicationController
+  
+  before_action :set_store, except: [:index, :new, :create]
   def index
+    @stores = Store.all
   end
 
   def show
   end
 
   def new
+    @store = Store.new
+    render partial: "form"
+  end
+
+  def create
+    @store = Store.new(store_params)
+    if @store.save
+      redirect_to stores_path
+    else 
+      render :new
+    end
   end
 
   def edit
+    render partial: "form"
   end
+
+  def update
+    @store = Store.find(params[:id])
+    if @store.update(store_params)
+    redirect_to stores_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @store.destroy
+    redirect_to stores_path
+  end
+
+
+    private
+
+    def set_store
+      @store = Store.find(params[:id])
+    end
+
+    def store_params
+      params.require(:store).permit(:name)
+    end
+
+
 end
--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,13 +1,57 @@
 class ThingsController < ApplicationController
+  before_action :set_store
+  before_action :set_thing, except: [:index, :new, :create]
+  
   def index
+    @things = @stores.things
   end
 
   def show
   end
 
   def new
+    @thing = @store.things.new
+    render partial: "form"
+  end
+  
+  def create
+    @thing = @store.things.new(thing_params)
+    if @thing.save
+      redirect_to store_thing_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    # render partial: "form"
   end
 
-  def edit
+  def update
+    if @thing.update
+      redirect_to store_things_path
+    else 
+      render :edit
+    end
   end
+
+  def destroy
+    @thing.destroy
+    redirect_to store_things_path
+  end
+
+  private
+  
+    def set_store
+      @stores = Store.find(params[:store_id])
+    end
+
+    def set_thing
+      @thing = Thing.find(params[:id])
+    end
+
+    def thing_params
+      params.require(:thing).permit(:name, :price, :color)
+    end
+    
 end
